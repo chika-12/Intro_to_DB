c423b43af60e351660ff6524271c555324dfc40d
chika-12 <markworship001@gmail.com>
2025-10-17 05:20:45 +0100

    table description

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,7 @@
-USE alx_book_store;
-SHOW CREATE TABLE Books;
+-- task_4.sql
+-- Prints the full description of the table Books in the alx_book_store database
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
