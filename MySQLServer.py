efb89a6c18fbcc36beac4b2c3c27a0b840417d03
chika-12 <markworship001@gmail.com>
2025-10-17 04:40:27 +0100

    python script for db connection

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import Error
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+try:
+  
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Chika12mark??",
+    port=33306
+  )
+
+  if mydb.is_connected():
+    cursor = mydb.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXITS alx_book_store")
+    logging.info(" Database 'alx_book_store' created successfully!")
+  cursor.close()
+  mydb.close()
+except Error as e:
+  logging.info("Connection failed")
+
